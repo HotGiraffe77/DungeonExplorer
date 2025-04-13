543ce5217a614a1cabd8c8376538af9aebfa9ea6
HotGiraffe7 <dmcross@live.com>
2025-04-13 17:57:31 +0100

    updated testing class

--- Test.cs
+++ Test.cs
@@ -11,34 +11,47 @@ namespace DungeonExplorer
     /// </summary>
     internal class Test
     {
-        public Player TestPlayer { get; private set; }
-        public Room TestRoom { get; private set; }
-        public Inventory TestInventory { get; private set; }
-
         // Contains the tests that are run at the start of the program.
         public void RunTests()
         {
+
             Console.WriteLine("Running tests...");
-
-            //// Testing the username.
-            //TestPlayer = new Player("Username", 10, 10);
-            //Debug.Assert(TestPlayer.Name == "Username", "Player name should be Username.");
-            //// Tests that the inventory starts empty.
-            //Debug.Assert(TestInventory.InventoryContents() == "Nothing :(", "A new players inventory should be empty.");
-            //// Tests that the items picked up are moved to the inventory.
-            //TestInventory.PickUpItem("Health Potion");
-            //Debug.Assert(TestInventory.InventoryContents().Contains("Health Potion"), "Inventory should have a health potion.");
-            //// Testing the room description.
-            //TestRoom = new Room();
-            //string RoomDescription = TestRoom.GetDescription();
-            //Debug.Assert(!string.IsNullOrEmpty(RoomDescription), "The Room description shoudln't be empty.");
-            //// Testing the rooms item.
-            //string RoomItem = TestRoom.GetItems();
-            //Debug.Assert(!string.IsNullOrEmpty(RoomItem), "The room item shouldn't be empty");
-
-            //Console.WriteLine("All tests finished successfully.");
+            TestPlayer();
+            TestCombat();
 
 
+        }
+
+
+        private void TestPlayer()
+        {
+            Player player = new Player("Test", 100, 15);
+
+            Debug.Assert(player.Name == "Test", "Player name incorrect...");
+            Debug.Assert(player.Health == 100, "Player health incorrect...");
+            Debug.Assert(player.Damage == 15, "Player Damage incorrect...");
+
+            Trace.WriteLine("Player methods tested successfully...");
+        }
+
+        private void TestCombat()
+        {
+            Player player = new Player("Test", 100, 15);
+            Monster monster = new Monster("TestMonster", 20, 1);
+
+            while (player.IsAlive && monster.IsAlive)
+            {
+                player.Attack(monster);
+                if (monster.IsAlive)
+                {
+                    monster.Attack(player);
+                }
+            }
+
+            Debug.Assert(player.IsAlive, "Player should have survived...");
+            Debug.Assert(!monster.IsAlive, "Monster should have died...");
+
+            Trace.WriteLine("Combat tested successfully.");
 
         }
     }
